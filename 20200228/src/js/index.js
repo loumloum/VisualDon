b7ae2bb251eaeebebcc624a50ce3f1549a3059c4
Laurie Pouly <laurie.pouly@heig-vd.ch>
2020-03-06 09:46:30 +0100

    Ajout exercice 3 + dossier du jour

--- 20200228/src/js/index.js
+++ 20200228/src/js/index.js
@@ -1,26 +1,58 @@
 import * as d3 from 'd3'
 
-const data = require('/data/data-tattoo.json')
-
 const WIDTH = 1000
 const HEIGHT = 500
-// const container = DOM.svg(WIDTH, HEIGHT)
+const MARGIN = 5
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', WIDTH)
+  .attr('height', HEIGHT)
+
+const DATA = [
+  { nom: 'Lausanne', population: 138905 },
+  { nom: 'Yverdon', population: 30143 },
+  { nom: 'Montreux', population: 26574 },
+  { nom: 'Renens', population: 21036 },
+  { nom: 'Nyon', population: 20533 },
+  { nom: 'Vevey', population: 19827 },
+]
+
+const MARGIN_LEFT = 100
+const MARGIN_BOTTOM = 50
+const BAR_WIDTH = (WIDTH - MARGIN_LEFT) / DATA.length
 
 
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(DATA, d => d.population)])
+  .range([HEIGHT - MARGIN_BOTTOM, 0])
 
-// const MARGIN = 5 // espace entre les bâtons
-// const BAR_WIDTH = WIDTH / DATA.length // largeur des bâtons
+const batons = svg.append('g')
+  .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
 
-// const svg = d3.select(container)
+batons.selectAll('rect')
+  .data(DATA)
+  .enter()
+  .append('rect')
+  .attr('x', (d, i) =>  i * BAR_WIDTH)
+  .attr('width', BAR_WIDTH - MARGIN)
+  .attr('y', d => yScale(d.population))
+  .attr('height', d => HEIGHT - MARGIN_BOTTOM - yScale(d.population))
+  .attr('fill', 'steelblue')
 
-
-const body = d3.select('body')
-     .attr('width', WIDTH)
-     .attr('height', HEIGHT)
-
-const ul = body.append('ul')
-const li = ul.selectAll('li')
-    .data(data)
+  batons.selectAll('text')
+    .data(DATA)
     .enter()
-    .append('li')
-    .text(d => d.name)
+    .append('text')
+    .text(d => d.nom)
+    .attr('x', (d, i) =>  i * BAR_WIDTH + BAR_WIDTH / 2)
+    .attr('y', HEIGHT - MARGIN_BOTTOM / 2)
+    .attr('text-anchor', 'middle')
+
+const axis = d3.axisLeft(yScale)
+  .ticks(5)
+  .tickFormat(d => `${d / 1000}k`)
+
+const gAxis = svg.append('g')
+  .attr('transform', `translate(${MARGIN_LEFT  * 0.7}, 0)`)
+  .call(axis)
